3c217469cb92a3cb8bd52de88388194abaed84b6
SUMANKC7 <sumankc844@gmail.com>
2024-03-11 21:23:43 +0545

    Scrolling view in the Weather forcast

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -22,78 +20,143 @@ class WeatherScreen extends StatelessWidget {
         ),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: (){},
-           icon: const Icon(Icons.refresh),
-           highlightColor: const Color.fromARGB(255, 128, 224, 250),
-           ),
-           
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+            highlightColor: const Color.fromARGB(255, 128, 224, 250),
+          ),
         ],
       ),
-      
-      body:  Padding(
-        padding:const EdgeInsets.all(20.0),
-        child:  Column(
-             children: [
-        //main card
-             SizedBox(
-        width: double.infinity,
-         child: Card(
-          elevation: 9,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(19),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  
-                  children: [
-                    Text("300.67°F",style: TextStyle(
-                        fontSize: 32,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 9,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(19),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "300.67°F",
+                            style: TextStyle(
+                              fontSize: 32,
+                            ),
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 74,
+                          ),
+                          Text(
+                            "Rain",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    ),
-                   
-                    Icon(Icons.cloud,size: 74,),
-                    Text("Rain",style: TextStyle( fontSize: 20,),
-                    ),
-                      
-                  ],
-                       
+                  ),
                 ),
               ),
             ),
-          ),
-         ),
-             ),
-        
-        const SizedBox(height: 20,),
-       const Align(
-        alignment: Alignment.centerLeft,
-          child:  Text("Weather Forcast",
-          style:TextStyle(
-            fontSize: 29,
-          fontWeight: FontWeight.bold ,
-          ) ,
-          ),
+
+            const SizedBox(
+              height: 20,
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Weather Forcast",
+                style: TextStyle(
+                  fontSize: 29,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                WeatherForcast(),
+               WeatherForcast(),
+                WeatherForcast(),
+                  WeatherForcast(),
+                  WeatherForcast(),
+                ],
+              ),
+            ),
+
+          
+            //Additional Information
+            const SizedBox(
+              height: 40,
+            ),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
         ),
-         const SizedBox(height: 20,),
-             //Weather forcast
-       const Placeholder(
-         fallbackHeight: 150,
+      ),
+    );
+  }
+}
+
+class WeatherForcast extends StatelessWidget {
+  const WeatherForcast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Card(
+      elevation: 8,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
         ),
-        
-        //Additional Information
-        const SizedBox(height: 40,),
-       const  Placeholder(
-        fallbackHeight: 150,
+        child: const Column(
+          children: [
+            Text(
+              "09:00",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Icon(
+              Icons.cloud,
+              size: 32,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              "320.12",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
-        
-             ],
-        ),
-      ) ,
+      ),
     );
   }
 }
